af0158e2a15658a76e478bddd4cdd0ee5eb701d7
MdMannanDewan <mdmannandewan.mbstu@gmail.com>
2025-06-17 23:32:20 +0600

    Min and Max Subarray sum added

--- Array/SubArrays.java
+++ Array/SubArrays.java
@@ -17,8 +17,45 @@ public class SubArrays {
         }
     }
 
+    public static int maxSubarraySum(int[] arr) {
+        int max = Integer.MIN_VALUE, prefix[] = new int[arr.length];
+        prefix[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+        for (int start = 0; start < arr.length; start++) {
+            for (int end = start; end < arr.length; end++) {
+                int currentSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+                if (currentSum > max) {
+                    max = currentSum;
+                }
+            }
+        }
+        return max;
+    }
+
+    public static int minSubarraySum(int[] arr) {
+        int min = Integer.MAX_VALUE, prefix[] = new int[arr.length];
+        prefix[0] = arr[0];
+        // Generate Prefix Array
+        for (int i = 1; i < arr.length; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+        // get minimum subarray sum
+        for (int start = 0; start < arr.length; start++) {
+            for (int end = start; end < arr.length; end++) {
+                int subArraySum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+                min = min > subArraySum ? subArraySum : min;
+            }
+        }
+        return min;
+    }
+
     public static void main(String[] args) {
         int arr[] = { 2, 4, 6, 8, 10 };
-        printSubArrays(arr);
+        int arr1[] = { 1, 2, -6, 1, -3 };
+        // printSubArrays(arr);
+        int min = minSubarraySum(arr1);
+        System.out.println(min);
     }
 }
